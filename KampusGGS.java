00924a99b44f60dcf4bf0ec0730b6b9fc2d11eb2
Mohammad Rizqi Aulia Abdillah <148023035+mrzqiaullah27@users.noreply.github.com>
2024-06-04 21:23:45 +0700

    menambahkan fungsi untuk menghitung serta mencetak bobot minimum dari MST yang ditemukan

--- KampusGGS.java
+++ KampusGGS.java
@@ -70,3 +70,28 @@ void union(Subset subsets[], int x, int y) {
             subsets[v].parent = v;
             subsets[v].rank = 0;
         }
+        i = 0;
+
+        while (e < V - 1) {
+            if (i >= E) {
+                System.out.println("TIDAK MEMUNGKINKAN");
+                return;
+            }
+
+            Edge next_edge = edge[i++];
+
+            int x = find(subsets, next_edge.src);
+            int y = find(subsets, next_edge.dest);
+
+            if (x != y) {
+                result[e++] = next_edge;
+                union(subsets, x, y);
+            }
+        }
+
+        int minimumCost = 0;
+        for (i = 0; i < e; ++i)
+            minimumCost += result[i].weight;
+        System.out.println(minimumCost);
+    }
+
